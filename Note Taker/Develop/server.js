bb4093c5ab68c7aa18bf90bc5f016e58e171d63a
Michael DiSanto <mdisanto928@gmail.com>
2021-05-11 15:25:09 -0400

    added code server.js

--- Note Taker/Develop/server.js
+++ Note Taker/Develop/server.js
@@ -1,6 +1,7 @@
 // Dependencies
 var express = require ('express');
 var path = require ('path');
+var fs = require ('fs');
 
 // Sets up the Express App
 const app = express ();
@@ -9,3 +10,16 @@ const PORT = process.env.PORT || 3000;
 // Sets up the express app to handle parsing
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+
+// Basic routes that sends the user first to the AJAX Page
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+
+app.get('/api/notes', (req, res) => res.sendFile(path.join(__dirname, 'notes.html')));
+
+app.post('/api.notes'), (req, res) =>
+
+app.delete('/api/notes'), (req, res) =>
+
+
+
+
