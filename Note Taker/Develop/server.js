1bdf16e947293b1c63a9c625b2ec070f840bf9dd
Michael DiSanto <mdisanto928@gmail.com>
2021-05-11 17:30:59 -0400

    line 18 works now. Working on line 20

--- Note Taker/Develop/server.js
+++ Note Taker/Develop/server.js
@@ -15,11 +15,11 @@ app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
 // Basic routes that sends the user first to the AJAX Page
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public','index.html')));
 
-app.get('/api/notes', (req, res) => res.sendFile(path.join(__dirname, 'notes.html')));
+app.get('/api/notes', (req, res) => res.sendFile(path.join(__dirname, 'public', 'notes.html')));
 
-app.post('/api.notes', (req, res) => {
+app.post('/api/notes', (req, res) => {
     const saveNote = req.body;
     console.log (saveNote);
     newNote.push (saveNote);
